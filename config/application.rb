41b1f66605ab0315fc69e753c4d4f40eaf6de44f
alejandra <alejandra@tangosource.com>
2014-07-05 22:40:36 -0500

    Setting up bower to serve assets

--- config/application.rb
+++ config/application.rb
@@ -8,10 +8,11 @@ Bundler.require(*Rails.groups)
 
 module Foursquare
   class Application < Rails::Application
+    config.assets.paths << Rails.root.join('vendor', 'assets', 'components')
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
